c3069177d949ad0444895d50080b123fca7d9300
atiq.masood <atiq.masood@mi-c3.com>
2018-04-14 12:46:40 +0500

    env setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Recipe Website</h1>
+                <h1>Recipe Website working good.</h1>
             </div>
         );
     }
